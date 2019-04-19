2e4f38c272fea52856a564b465b59c53da552a95
Omar <maciaseo@gmail.com>
2019-04-19 13:26:41 -0500

    [AB#20] POCO creation
    User account POCO creation.

---
+++ StWebApi/StWebApi/Pocos/UserAccount.cs
@@ -0,0 +1,10 @@
+﻿namespace StWebApi.Pocos
+{
+    public class UserAccount
+    {
+        public int Id { get; set; }
+        public string CreationDate { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
